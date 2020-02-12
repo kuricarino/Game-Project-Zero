5551b2b3beac4af5b70ac2428d5dc01325f641d0
Kristine Ilano <kuri.carino@Kristines-MacBook-Pro.local>
2020-02-11 17:52:38 -0800

    Got functions and eventListeners working; possible forseeable issues with scoring

--- js/app.js
+++ js/app.js
@@ -37,7 +37,7 @@ const triviaQA = [
     },
 
     {
-        question: "Does Ethan’s wife die in the series?",
+        question: "Does Ethan’s wife, Julia, die in the series?",
         answersArray: [
             "True",
             "False"
@@ -47,15 +47,15 @@ const triviaQA = [
     },
 
     {
-        question: "The character William Brandt, played by Jeremy Renner, also stars in all of the films below except for:",
+        question: "The character William, played by Jeremy Renner, also stars in all of the films below except for:",
         answersArray: [
-            "The Bourne Legacy",
-            "The Knockout",
+            `The King's Speech`,
             "S.W.A.T.",
+            "The Bourne Legacy",
             "The Hurt Locker",
         ],
         attemptedAnswer: false,
-        correctAnswer: "The Knockout",
+        correctAnswer: `The King's Speech`,
     },
 
     {
@@ -71,28 +71,31 @@ const triviaQA = [
         correctAnswer: "Protocol"
     },
 
-    userAnswers = [
-        //empty array
-    ],
+    // userAnswers = [] rendered useless by function clickAnswer below
 
     userScore = 0,
+    maxWrong = 0,
+    //maxScore = 5,
 ];
 
 // password QA
-const passwordObject = {
+const password = {
+
+    passwordInstructions: [`Use ONLY 5 LETTERS from the correct answers and the following clue.`],
 
     passwordQuestion: [
-        "In the scene shown at the beginning of the game, Ethan leaves behind something on the computer desk. What did he leave? Using letters from the correct answers above, enter the password below."
+        `In the scene shown at the beginning of the game, Ethan leaves behind something on the computer desk. 
+        What did he leave?`
     ],
 
+    passwordClues: [`Ethan Hunt, TuraNdot, False, The KIng's Speech, Protocol`],
+
     passwordAnswer: ["knife"]
+
 };
 
-// ***** FUNCTIONS ***** //
-
 // on F1 - F5 CLICK
-
-let displayedQuestion = 0;
+let displayedQuestion = 0; // used displayedQuestion in clickAnswer function
 
 function question(event) {
     if (event.target.innerHTML === "F1") {
@@ -134,74 +137,144 @@ function question(event) {
             }
         
         question.appendChild(answers);
+        document.querySelector('ol').addEventListener('click', clickAnswer);
         
 };
-
 
 // add event listener for F1 - F5 buttons
 document.querySelectorAll(".question").forEach(function(button) {
     button.addEventListener("click", question)
 });
 
-
-// add event listener for user click on answer list
-let targetElement;
-
-function addCustomEventListener (selector, event, handler) {
-let rootElement = document.querySelector("#question-answers");
-    rootElement.addEventListener(event, function(evt){
-        targetElement = evt.target
-        //let selector = 'li';
-        while (targetElement != null) {
-            if (targetElement.matches(selector)) {
-                handler(evt);
-                userAnswers.push(targetElement.innerText);
-                return;
-            }
-            targetElement = targetElement.parentElement;
-        }
-    }, true
-    );
-
-}
-
-addCustomEventListener('li.answer', 'click', taskClickHandler);
-
-
-// function that checks correct answers
-
-function taskClickHandler() {
-    if (targetElement.innerText = "Ethan Hunt") {
-        userScore += 1;
-        console.log(userScore);
+// click event targets li and checks innerText against triviaQA array's correctAnswer at displayedQuestion
+// can console log score, mission complete and mission failed.
+function clickAnswer (event) {
+    if (event.target.innerText === triviaQA[displayedQuestion].correctAnswer) {
+        console.log(userScore += 1);
     } else {
-        console.log(`wrong answer`);
+        console.log(maxWrong += 1);
+            if (maxWrong === 3) {
+        console.log(`mission failed`);
+    }
+        // if (userScore >= 3) {
+        //     console.log(`mission complete`);
+
+        //         // pWdiv and passwordButton propogates if userScore = 5
+        //         // addEventListener
+
+        //         //don't need separate div?
+        //         // let pWdiv = document.createElement("div");
+        //         //     pWdiv.setAttribute("class", "password-div");
+        //         //     document.getElementById("question-answers").appendChild(pWdiv);
+
+        //         let passwordButton = document.createElement("button");
+        //             passwordButton.setAttribute("id", "password");
+        //             passwordButton.innerHTML = "FILES READY TO DOWNLOAD";
+        //             document.getElementById("blank-computer-screen").appendChild(passwordButton);
+                
+        //             document.getElementById("password").addEventListener('click', clickPassword);
+        // }
+}
+missionComplete();
+};
+
+
+
+function missionComplete() {
+if (userScore >= 5) {
+    console.log(`mission complete`);
+
+        // pWdiv and passwordButton propogates if userScore = 5
+        // addEventListener
+
+        //don't need separate div?
+        // let pWdiv = document.createElement("div");
+        //     pWdiv.setAttribute("class", "password-div");
+        //     document.getElementById("question-answers").appendChild(pWdiv);
+
+        let passwordButton = document.createElement("button");
+            passwordButton.setAttribute("id", "password");
+            passwordButton.innerHTML = "FILES READY TO DOWNLOAD";
+            document.getElementById("blank-computer-screen").appendChild(passwordButton);
+        
+            document.getElementById("password").addEventListener('click', clickPassword);
+}
+};
+
+
+// opens up password state
+function clickPassword(event) {
+    if (event.target.innerHTML === "FILES READY TO DOWNLOAD") {
+
+        //remove button
+        let removeFilesDownloadButton = document.getElementById('password');
+            removeFilesDownloadButton.remove();
+        
+        let passwordClue;
+        let check = document.querySelector("h1");
+
+        if (check == null) {
+            passwordClue = document.createElement("h1");
+            passwordClue.innerHTML = `${password.passwordInstructions} + 
+            ${password.passwordClues} + 
+            ${password.passwordQuestion}`;
+            passwordClue.setAttribute("class", "password");
+            document.getElementById("blank-computer-screen").appendChild(question);
+        } else {
+            check.remove();
+            passwordClue = document.createElement("h1");
+            passwordClue.innerHTML = `${password.passwordInstructions} +
+            ${password.passwordClues} +
+            ${password.passwordQuestion}`;
+            passwordClue.setAttribute("class", "password");
+            document.getElementById("blank-computer-screen").appendChild(passwordClue);
+        }
+
+        let passwordInput = document.createElement("input");
+            passwordInput.setAttribute("type", "text");
+            passwordInput.setAttribute("minlength", "5");
+            passwordInput.setAttribute("maxlength", "5");
+            passwordInput.innerHTML = "5 LETTER WORD";
+            //passwordInput.setAttribute("spellcheck", "true") 
+            document.getElementById("blank-computer-screen").appendChild(passwordInput);
+
+        let passwordInputButton = document.createElement("button");
+            passwordInputButton.innerHTML = "Enter"
+            document.getElementById("blank-computer-screen").appendChild(passwordInputButton);
+
+        if (passwordInput.innerHTML === 'knife' || "KNIFE" || "Knife") {
+            console.log(`correct`);
+
+        }   
     }
 };
 
-// }
-// if (userAnswers.includes('Ethan Hunt')) {
-//     userScore += 1;
-//     console.log(userScore);
-// }
-// function that checks correct answers
-// function userAnswersCheck () {
-//     if (userAnswers.includes("Ethan Hunt")) {
-//         console.log(`true`);
-//     }
-//     // } else if (event.target.innerHTML === "F2") {
-//     //     displayedQuestion = 1;
-//     // } else if (event.target.innerHTML === "F3") {
-//     //     displayedQuestion = 2;
-//     // } else if (event.target.innerHTML === "F4") {
-//     //     displayedQuestion = 3;
-//     // } else if (event.target.innerHTML === "F5") {
-//     //     displayedQuestion = 4;
-//    // }
-// };
 
-// in object create one for wrong answers
-    // check if it equals 3 
-    // gif of fire
 
-//userAnswersCheck();
+
+
+// // creates new div, propogates passwordInput, passwordButton and passwordInstruct and Clues
+// let pWdiv = document.createElement("div");
+// pWdiv.setAttribute("class", "password-div");
+// document.getElementById("blank-computer-screen").appendChild(pWdiv);
+
+// let passwordInstruct = document.createElement("h2");
+// passwordInstruct.innerHTML = `ENTER a 5 LETTER password. Use LETTERS from the correct answers and the clue below.` 
+// pWdiv.appendChild(passwordInstruct);
+
+// let passwordClue = document.createElement("h3");
+// passwordClue.innerHTML = `Ethan Hunt, TuraNdot, False, The KIng's Speech, Protocol`;
+// pWdiv.appendChild(passwordClue);
+
+// let passwordClueTwo = document.createElement("h3");
+// passwordClueTwo.innerHTML = `At the end of the clip, what did Ethan leave behind on the desk?`
+// pWdiv.appendChild(passwordClueTwo);
+
+// let passwordInput = document.createElement("input");
+// passwordInput.setAttribute("type", "text");
+// passwordInput.setAttribute("minlength", "5");
+// passwordInput.setAttribute("maxlength", "5");
+// //passwordInput.setAttribute("spellcheck", "true") 
+// pWdiv.appendChild(passwordInput);
+
+
